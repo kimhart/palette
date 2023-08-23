12e7a8ef60de2b2ae9fa57dc297dcb565d63e5ea
Kim Hart <kimhart@Kims-Laptop.local>
2023-08-23 19:56:12 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `palette`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "wOmkQspKFshMmOyJxSoizRkqucJ4yv1UCYBjHTV4zcQ",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
